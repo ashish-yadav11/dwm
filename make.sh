f25e9258a070636d7edb9621735333d73da5c8d3
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-08 17:47:45 +0530

    Updated make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/dash
 pkgname=dwm
 pkgver=6.2
-tar -zcvf ${pkgname}-${pkgver}.tar.gz ${pkgname}-${pkgver}
+tar -zcvf "${pkgname}-${pkgver}.tar.gz" "${pkgname}-${pkgver}"
 makepkg -cfsi
