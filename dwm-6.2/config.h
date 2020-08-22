ce0095f984783756e87545dba6dc7d64e22a9806
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-08-23 01:23:24 +0530

    Improved colors and comments in config.h

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -13,12 +13,16 @@ static const int gappih                         = 1;    /* horiz inner gap betwe
 static const int gappiv                         = 1;    /* vert inner gap between windows */
 static const int gappoh                         = 1;    /* horiz outer gap between windows and screen edge */
 static const int gappov                         = 1;    /* vert outer gap between windows and screen edge */
-static const unsigned int systraypinning        = 0;    /* 0: sloppy systray follows selected monitor, >0:pin systray to monitor X */
+
+/* 0 means systray follows selected monitor; n other than 0 means pin systray to nth monitor */
+static const unsigned int systraypinning        = 0;
 static const unsigned int systrayspacing        = 4;    /* systray spacing */
-static const int systraypinningfailfirst        = 1;    /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray                    = 1;    /* 0 means no systray */
-static const int showbar                        = 1;    /* 0 means no bar */
-static const int topbar                         = 1;    /* 0 means bottom bar */
+
+/* 1 means if pinning fails, display systray on the first monitor; 0 means display systray on the last monitory */
+static const int systraypinningfailfirst        = 1;
+static const int showsystray                    = 1; /* 0 means no systray */
+static const int showbar                        = 1; /* 0 means no bar */
+static const int topbar                         = 1; /* 0 means bottom bar */
 /*  Display modes of the tab bar: never shown, always shown, shown only in  */
 /*  monocle mode in the presence of several windows.                        */
 /*  Modes after showtab_nmodes are disabled.                                */
@@ -41,15 +45,13 @@ static const char col_white1[]          = "#eeeeee";
 static const char col_white2[]          = "#dddddd";
 static const char col_red[]             = "#b21e19";
 static const char col1[]                = "#8fb4a6"; /* default icon color */
-static const char col2[]                = "#c8c061"; /* warning icon color */
-static const char col3[]                = "#62a6c8"; /* mail block - frozen */
-static const char col4[]                = "#ac90c8"; /* mail block - MAILSYNC started */
-static const char col5[]                = "#d28777"; /* mail block - syncing */
-static const char col6[]                = "#beb65d"; /* mail block - last sync failed */
+static const char col2[]                = "#beb873"; /* warning icon color */
+static const char col3[]                = "#8d97cd"; /* mail block - frozen */
+static const char col4[]                = "#b399cd"; /* mail block - MAILSYNC started */
+static const char col5[]                = "#d29b88"; /* mail block - syncing */
 
 enum { SchemeStts, SchemeCol1, SchemeCol2, SchemeCol3, SchemeCol4,
-       SchemeCol5, SchemeCol6, SchemeNorm, SchemeSel, SchemeUrg,
-       SchemeLtSm, SchemeTray }; /* color schemes */
+       SchemeCol5, SchemeNorm, SchemeSel, SchemeUrg, SchemeLtSm, SchemeTray }; /* color schemes */
 
 static const char *colors[][3] = {
         /*                  fg            bg              border   */
@@ -59,7 +61,6 @@ static const char *colors[][3] = {
 	[SchemeCol3]    = { col3,         col_black,      col_gray2 },
 	[SchemeCol4]    = { col4,         col_black,      col_gray2 },
 	[SchemeCol5]    = { col5,         col_black,      col_gray2 },
-	[SchemeCol6]    = { col6,         col_black,      col_gray2 },
 	[SchemeNorm]    = { col_white1,   col_gray1,      col_gray2 },
 	[SchemeSel]     = { col_white1,   col_cyan,       col_cyan },
 	[SchemeUrg]     = { col_white1,   col_red,        col_red },
