cbd25818894b6d4fc879b2b38a8f15e360a91a22
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-24 00:44:37 +0530

    Reordered config.mk

--- dwm-6.2/config.mk
+++ dwm-6.2/config.mk
@@ -26,9 +26,9 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 CPPFLAGS = -D_DEFAULT_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 #CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
-#CFLAGS   = -std=gnu99 -Wall -Wextra -Wno-unused-parameter -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
-#CFLAGS   = -std=gnu99 -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 CFLAGS   = -g -std=gnu99 -Wall -Wno-deprecated-declarations -Og ${INCS} ${CPPFLAGS}
+#CFLAGS   = -std=gnu99 -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
+#CFLAGS   = -std=gnu99 -Wall -Wextra -Wno-unused-parameter -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
 # compiler and linker
