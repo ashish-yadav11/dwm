885e946aca9ab46ad108ef4da6c685cd52b6c56d
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-27 14:49:57 +0530

    Cleanup - macros

--- dwm-6.2/util.h
+++ dwm-6.2/util.h
@@ -1,25 +1,33 @@
 /* See LICENSE file for copyright and license details. */
 
 //#define MAX(A, B)               ((A) > (B) ? (A) : (B))
-#define MAX(A, B) \
-   ({ __auto_type __A = (A); \
-       __auto_type  __B = (B); \
-     __A > __B ? __A : __B; })
 //#define MIN(A, B)               ((A) < (B) ? (A) : (B))
-#define MIN(A, B) \
-   ({ __auto_type __A = (A); \
-       __auto_type  __B = (B); \
-     __A < __B ? __A : __B; })
 //#define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
-#define BETWEEN(X, A, B) \
-   ({ __auto_type __X = (X); \
-       A <= __X && __X <= B; })
-#define SWAP(A, B) \
-   ({ __auto_type __A = (A); \
-       A = B; B = __A; })
+#define MAX(A, B)               ({ \
+                                        __auto_type __A = (A); \
+                                        __auto_type  __B = (B); \
+\
+                                        __A > __B ? __A : __B; \
+                                })
+#define MIN(A, B)               ({ \
+                                        __auto_type __A = (A); \
+                                        __auto_type  __B = (B); \
+\
+                                        __A < __B ? __A : __B; \
+                                })
+#define BETWEEN(X, A, B)        ({ \
+                                        __auto_type __X = (X); \
+\
+                                        A <= __X && __X <= B; \
+                                })
+#define SWAP(A, B)              ({ \
+                                        __auto_type __A = (A); \
+\
+                                        A = B; B = __A; \
+                                })
 
-#define STR_HELPER(x) #x
-#define STR(x) STR_HELPER(x)
+#define STR_HELPER(x)           #x
+#define STR(x)                  STR_HELPER(x)
 
 void die(const char *fmt, ...);
 void *ecalloc(size_t nmemb, size_t size);
