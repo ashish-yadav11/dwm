15dadc60d00690329cbbbdae3bedc1fe3e2e2db0
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2020-12-27 14:53:50 +0530

    Styling improvement - util.h

--- dwm-6.2/util.h
+++ dwm-6.2/util.h
@@ -1,8 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 
-//#define MAX(A, B)               ((A) > (B) ? (A) : (B))
-//#define MIN(A, B)               ((A) < (B) ? (A) : (B))
-//#define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
+/*
+#define MAX(A, B)               ((A) > (B) ? (A) : (B))
+#define MIN(A, B)               ((A) < (B) ? (A) : (B))
+#define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
+*/
 #define MAX(A, B)               ({ \
                                         __auto_type __A = (A); \
                                         __auto_type  __B = (B); \
