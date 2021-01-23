fecec9a45ff52d5c9e22d82557e17654e6132a4d
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-01-23 15:00:12 +0530

    Cleanup

--- dwm-6.2/dwm.c
+++ dwm-6.2/dwm.c
@@ -2359,7 +2359,7 @@ setup(void)
 
         /* be the child subreaper */
         if (prctl(PR_SET_CHILD_SUBREAPER, 1) == -1)
-		fputs("warning: could not set dwm as the subreaper\n", stderr);
+		fputs("warning: could not set dwm as subreaper\n", stderr);
 
 	/* init screen */
 	screen = DefaultScreen(dpy);
