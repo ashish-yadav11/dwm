9fe8341d76d8f5c05502c29f14404d3dc6c0f868
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-01-26 16:41:30 +0530

    Updated keybindings

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -274,7 +274,6 @@ static Key keys[] = {
 	{ MODLKEY|ShiftMask,            XK_F10,         spawn,                  SCRIPT1("systemctl_timeout.sh", "toggle") },
 	{ MODLKEY|ControlMask,          XK_F10,         spawn,                  SCRIPT1("systemctl_timeout.sh", "status") },
 	{ SUPKEY,                       XK_b,           spawn,                  SCRIPT0("gbtns.sh") },
-	{ SUPKEY,                       XK_n,           spawn,                  TERMCMD("newsboat -q") },
 	{ SUPKEY,                       XK_r,           spawn,                  TERMCMD("ranger --cmd='set show_hidden=false'") },
 	{ SUPKEY|ShiftMask,             XK_r,           spawn,                  TERMCMD("ranger") },
 	{ SUPKEY,                       XK_t,           spawn,                  TERMCMD("htop") },
