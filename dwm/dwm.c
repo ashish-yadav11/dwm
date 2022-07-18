9797e1bc7dd86c200ea1539eeafaa25657c38733
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2022-07-18 14:08:56 +0530

    Consistency

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -260,7 +260,7 @@ static void monocle(Monitor *m);
 static void motionnotify(XEvent *e);
 static void movemouse(const Arg *arg);
 static Client *nexttiled(Client *c);
-static void pop(Client *);
+static void pop(Client *c);
 static void propertynotify(XEvent *e);
 static void quit(const Arg *arg);
 static Monitor *recttomon(int x, int y, int w, int h);
